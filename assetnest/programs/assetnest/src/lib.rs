639f0e5e52bdea3af4f642f890e2eaeea54005d1
marotipatre <vaibhavspatre@gmail.com>
2024-06-26 00:59:53 +0530

    updated the program

--- assetnest/programs/assetnest/src/lib.rs
+++ assetnest/programs/assetnest/src/lib.rs
@@ -1,16 +1,105 @@
-use anchor_lang::prelude::*;
+use anchor_lang::{prelude::*};
+use anchor_spl::{
+    associated_token::AssociatedToken,
+    token::{mint_to, initialize_mint, InitializeMint, MintTo, Token},
+};
+use solana_program::pubkey;
 
 declare_id!("CQazXyCtxKTQrm85Z9c2FJjRYEVJmzhwwNNToRsBYKCZ");
 
 #[program]
-pub mod assetnest {
+mod assetnest {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_vault(ctx: Context<CreateVault>) -> Result<()> {
+        // Creating vault with mint token as quota token
+        anchor_lang::system_program::create_account(
+            CpiContext::new(
+                ctx.accounts.system_program.to_account_info(),
+                anchor_lang::system_program::CreateAccount {
+                    from: ctx.accounts.signer.to_account_info(),
+                    to: ctx.accounts.mint_token.to_account_info(),
+                },
+            ),
+            10_000_000, // lamports
+            82, // space
+            ctx.accounts.token_program.key,
+        )?;
+
+        // Initializing mint token
+        initialize_mint(
+            CpiContext::new(
+                ctx.accounts.token_program.to_account_info(),
+                InitializeMint {
+                    mint: ctx.accounts.mint_token.to_account_info(),
+                    rent: ctx.accounts.rent.to_account_info(),
+                },
+            ),
+            6, // decimals
+            ctx.accounts.signer.key,
+            Some(ctx.accounts.signer.key),
+        )?;
+
+        anchor_spl::associated_token::create(
+            CpiContext::new(
+                ctx.accounts.associate_token_program.to_account_info(),
+                anchor_spl::associated_token::Create {
+                    payer: ctx.accounts.signer.to_account_info(),
+                    associated_token: ctx.accounts.token_account.to_account_info(),
+                    authority: ctx.accounts.signer.to_account_info(),
+                    mint: ctx.accounts.mint_token.to_account_info(),
+                    system_program: ctx.accounts.system_program.to_account_info(),
+                    token_program: ctx.accounts.token_program.to_account_info(),
+                },
+            )
+        )?;
+
+        Ok(())
+    }
+
+    pub fn invest_vault(ctx: Context<InvestVault>, amount: u64) -> Result<()> {
+        mint_to(
+            CpiContext::new(
+                ctx.accounts.token_account.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.signer.to_account_info(),
+                    mint: ctx.accounts.mint_token.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                },
+            ),
+            amount,
+        )?;
+
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateVault<'info> {
+    #[account(mut)]
+    pub mint_token: Signer<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    ///CHECK:
+    #[account(mut)]
+    pub token_account: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associate_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
+
+#[derive(Accounts)]
+pub struct InvestVault<'info> {
+    #[account(mut)]
+    pub mint_token: Signer<'info>,
+    #[account(mut)]
+    pub signer: Signer<'info>,
+    ///CHECK:
+    #[account(mut)]
+    pub token_account: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    pub associate_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>,
+}
